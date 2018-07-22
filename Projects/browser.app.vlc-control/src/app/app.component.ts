f4e72734cda6533276012fb927f68de497c7858c
Richard Long <richard.long@nowhere>
2018-07-22 22:32:27 +0100

    remove 'contact' and 'tabs' pages

--- Projects/browser.app.vlc-control/src/app/app.component.ts
+++ Projects/browser.app.vlc-control/src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { TabsPage } from '../pages/tabs/tabs';
 import {HomePage} from "../pages/home/home";
 import {ControlPage} from "../pages/control/control";
 import {ConnectPage} from "../pages/connect/connect";
--- Projects/browser.app.vlc-control/src/app/app.module.ts
+++ Projects/browser.app.vlc-control/src/app/app.module.ts
@@ -4,9 +4,7 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -33,11 +31,9 @@ import {PlaylistPage} from "../pages/playlist/playlist";
     BrowseFolderPage,
     BrowseRootPage,
     ConnectPage,
-    ContactPage,
     ControlPage,
     HomePage,
     PlaylistPage,
-    TabsPage
   ],
   imports: [
     BrowserAnimationsModule,
@@ -60,11 +56,9 @@ import {PlaylistPage} from "../pages/playlist/playlist";
     BrowseFolderPage,
     BrowseRootPage,
     ConnectPage,
-    ContactPage,
     ControlPage,
     HomePage,
     PlaylistPage,
-    TabsPage
   ],
   providers: [
     StatusBar,
